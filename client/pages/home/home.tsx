98a96da67dcadc5b820ac72a05779ac5a9048eb9
Neos21 <neos21@gmail.com>
2026-09-24 08:26:19 +0900

    Update

--- client/pages/home/home.tsx
+++ client/pages/home/home.tsx
@@ -2,6 +2,7 @@ import { type ReactElement } from 'react';
 
 /** ログイン後のトップページ */
 export default function Home(): ReactElement {
+  /* eslint-disable neos-eslint-plugin/comment-colon-spacing */
   const bookmarkExampleCode = `javascript:(async () => {
   try {
     const policy = trustedTypes.createPolicy('neos21-ytf', { createScript: code => code });
@@ -14,7 +15,6 @@ export default function Home(): ReactElement {
     alert(\`スクリプト読み込みに失敗しました : \${error}\`);
   }
 })();`;
-  
   const tampermonkeyExampleCode = `// ==UserScript==
 // @name         Neo's YouTube Filter
 // @namespace    https://neos21.net/
@@ -29,6 +29,7 @@ export default function Home(): ReactElement {
 // @grant        none
 // @noframes
 // ==/UserScript==`;
+  /* eslint-enable neos-eslint-plugin/comment-colon-spacing */
   
   return (
     <main>
@@ -37,14 +38,14 @@ export default function Home(): ReactElement {
       <p className="mb-6">サイドメニューから各ページに移動してください。</p>
       
       <ul>
-        <li className="ml-6 mb-1 list-disc">ブックマークレットのサンプルコード</li>
+        <li className="mb-1 ml-6 list-disc">ブックマークレットのサンプルコード</li>
       </ul>
-      <p><textarea className="mb-4 textarea w-full" value={bookmarkExampleCode} rows={6} /></p>
+      <p><textarea className="textarea mb-4 w-full" value={bookmarkExampleCode} rows={6} /></p>  {/* eslint-disable-line @eslint-react/dom-no-script-url */}
       
       <ul>
-        <li className="ml-6 mb-1 list-disc">Tampermonkey のサンプルコード</li>
+        <li className="mb-1 ml-6 list-disc">Tampermonkey のサンプルコード</li>
       </ul>
-      <p><textarea className="mb-6 textarea w-full" value={tampermonkeyExampleCode} rows={6} /></p>
+      <p><textarea className="textarea mb-6 w-full" value={tampermonkeyExampleCode} rows={6} /></p>
       
       <p><a href="https://www.youtube.com" target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">YouTube</a></p>
     </main>
