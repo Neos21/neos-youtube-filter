214519740fa2b54bbc133a3468fa2d2a776c91ef
Neos21 <neos21@gmail.com>
2026-09-23 21:00:15 +0900

    V1

--- scripts/examples/tampermonkey-example.js
+++ scripts/examples/tampermonkey-example.js
@@ -1,13 +1,13 @@
-
-
 // ==UserScript==
 // @name         Neo's YouTube Filter
 // @namespace    https://neos21.net/
 // @version      2026-09-22
 // @description  Neo's YouTube Filter
+// @author       Neos21
 // @match        https://www.youtube.com/*
 // @match        https://m.youtube.com/*
 // @require      https://ytf.neos21.workers.dev/ytf.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @run-at       document-idle
 // @grant        none
 // @noframes
